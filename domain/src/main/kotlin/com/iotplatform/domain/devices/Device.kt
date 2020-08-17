22f433e7355f06fbf0fda8ef77137f2e80ae3813
Dmytro Yerusalimtsev <dmytro_yerusalimtsev@epam.com>
2020-08-18 00:43:31 +0300

    Initial project structure.

---
+++ domain/src/main/kotlin/com/iotplatform/domain/devices/Device.kt
@@ -0,0 +1,6 @@
+package com.iotplatform.domain.devices
+
+abstract class Device(
+        val id: Long,
+        val name: String
+)
---
+++ domain/src/main/kotlin/com/iotplatform/domain/devices/Speedometer.kt
@@ -0,0 +1,3 @@
+package com.iotplatform.domain.devices
+
+class Speedometer(id: Long, name: String) : Device(id, name)
