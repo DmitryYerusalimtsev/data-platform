2d88a64efd279b943782ec72dec9c1f47e155b31
Dmytro Yerusalimtsev <dmytro_yerusalimtsev@epam.com>
2020-08-18 18:07:13 +0300

    Added a skeleton for Ignite configuration.

--- infrastructure/k8s/ignite/install-ignite.sh
+++ infrastructure/k8s/ignite/install-ignite.sh
@@ -7,3 +7,5 @@ kubectl apply -f ignite-role-binding.yaml
 kubectl apply -f ignite-service.yaml
 kubectl apply -f ignite-config.yaml
 kubectl apply -f ignite-deployment.yaml
+
+#docker run -d -v /Users/dmytro_yerusalimtsev/Downloads/ignite_work_dir:/persistence -e IGNITE_WORK_DIR=/persistence -p 11211:11211 -p 47100:47100 -p 47500:47500 -p 49112:49112 -p 10800:10800 -p 10900:10900 apacheignite/ignite:2.8.1
