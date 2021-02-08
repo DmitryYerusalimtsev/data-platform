3c736fbaedfc188678b44336ce896984a2d299d4
Dmitry Yerusalimtsev <dima.yerusalimtsev@gmail.com>
2021-02-08 12:12:25 +0200

    Added config builder to application.

--- core/src/main/scala/com/iotplatform/core/config/TypesafeConfigProvider.scala
+++ core/src/main/scala/com/iotplatform/core/config/TypesafeConfigProvider.scala
@@ -5,8 +5,8 @@ import com.typesafe.config.{ConfigBeanFactory, ConfigFactory}
 import java.io.File
 import scala.reflect.{ClassTag, classTag}
 
-trait TypesafeConfigProvider {
-  def configFrom[C: ClassTag](path: String): C = {
+trait TypesafeConfigProvider extends ConfigProvider {
+  override def configFrom[C: ClassTag](path: String): C = {
     val config =
       if (path.isEmpty) ConfigFactory.load
       else ConfigFactory.parseFile(new File(path))
